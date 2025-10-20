39bc8578f775e1e4c46c37f965ff68dc49d4d64f
Aniket Kshirsagar <kshirsagarask19@gmail.com>
2025-10-20 09:55:07 -0400

    Add project changee

--- frontend/src/components/Layout/Topbar.jsx
+++ frontend/src/components/Layout/Topbar.jsx
@@ -9,13 +9,13 @@ const Topbar = () => {
       <div className="container mx-auto flex justify-between items-center py-3 px-4">
         {/* Left section - icons */}
         <div className="hidden md:flex items-center space-x-4">
-          <a href="https://www.facebook.com/paceuniversity" className="hover:text-gray-300">
+          <a href="" className="hover:text-gray-300">
             <TbBrandMeta className="h-5 w-5" />
           </a>
-          <a href="https://www.instagram.com/paceuniversity" className="hover:text-gray-300">
+          <a href="" className="hover:text-gray-300">
             <IoLogoInstagram className="h-5 w-5" />
           </a>
-          <a href="https://x.com/paceuniversity" className="hover:text-gray-300">
+          <a href="" className="hover:text-gray-300">
             <RiTwitterXLine className="h-4 w-4" />
           </a>
         </div>
